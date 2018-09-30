d75c4e29bb96845e00f80824088d2021b75fae0e
brianrsavage <bsavage@racc.edu>
2018-09-30 19:55:42 -0400

    done

---
+++ series/strings-position/index.php
@@ -0,0 +1,14 @@
+<?php
+
+  $find = 'is';
+
+  $find_length = strlen($find);
+
+  $string = 'This is a string, and it is an example.';
+
+  while($string_position = strpos($string, $find, $offset)){
+    echo '<strong>'.$find.'</strong> found at '.$string_position.'<br>';
+    $offset = $string_position+ $find_length;
+  }
+
+?>
