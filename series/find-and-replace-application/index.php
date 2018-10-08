bc3338461f1c23a72798d69528067866687d5e20
brianrsavage <bsavage@racc.edu>
2018-10-08 09:14:20 -0400

    updated question numbers

--- series/find-and-replace-application/index.php
+++ series/find-and-replace-application/index.php
@@ -1,27 +1,27 @@
 <?php
 
-  // Explain why we are creating the $offset variable and what it is being used for.
+  // 01.Explain why we are creating the $offset variable and what it is being used for.
   $offset = 0;
 
-  // Explain what we are doing in the if statement and why we would do it.
+  // 02.Explain what we are doing in the if statement and why we would do it.
   if(isset($_POST['text'])&&isset($_POST['searchfor'])&&isset($_POST['replacewith'])){
 
-    // Provide a reason why we would make a variable to capture a variable we could already use.
+    // 03.Provide a reason why we would make a variable to capture a variable we could already use.
     $text = $_POST['text'];
     $search = $_POST['searchfor'];
     $replace = $_POST['replacewith'];
 
-    // Explain why we need to create this variable $search_length.
+    // 04.Explain why we need to create this variable $search_length.
     $search_length = strlen($search);
 
       if(!empty($text)&&!empty($search)&&!empty($replace)){
 
         while ($strpos = strpos($text, $search, $offset)) {
 
-          // Explain why we change the value of the $offset variable here.
+          // 05.Explain why we change the value of the $offset variable here.
           $offset = $strpos + $search_length;
 
-          // Explain what we are doing here to the $text variable.
+          // 06.Explain what we are doing here to the $text variable.
           $text = substr_replace($text, $replace, $strpos, $search_length);
 
         }
@@ -30,7 +30,7 @@
 
       } else {
 
-        // Explain why we include this here.
+        // 07.Explain why we include this here.
         echo 'Please fill in all fields.';
 
       }
@@ -41,7 +41,7 @@
 
 <hr>
 
-<!-- Explain the action and method attributes. -->
+<!-- 08.Explain the action and method attributes. -->
 <form action="index.php" method="POST">
 
   <textarea name="text" rows="6" cols="30"></textarea><br><br>
@@ -52,7 +52,7 @@
   Replace with:<br>
   <input type="text" name="replacewith"><br><br>
 
-  <!-- Explain what the type and value attributes are and do. -->
+  <!-- 09.Explain what the type and value attributes are and do. -->
   <input type="submit" value="Find and Replace">
 
 </form>
