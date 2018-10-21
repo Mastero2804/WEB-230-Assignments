bb9b3aed102f6d74bde436a930bb621c1c66eedb
brianrsavage <bsavage@racc.edu>
2018-10-21 10:30:42 -0400

    updated timestamps

--- series/timestamps/index.php
+++ series/timestamps/index.php
@@ -1,11 +1,18 @@
 <?php
 
-  echo $time = time();
+  // 01. Explaine what the time function is.
+  $time = time();
 
-  echo '<br/>';
-
+  // 02. Explain what the following line of code is doing and how.
   $actual_time = date('D M Y @ H:i:s', $time);
 
-  echo 'The current dte/time is: '.$actual_time;
+  // 03. Explain what the following line of code is doing and how.
+  $time_modified = date('d M Y @ H:s', $time);
+
+  // 04. Explain a way the $actual_time might be used in a web application.
+  echo '<h3>The current dte/time is: '.$actual_time.'<h3>';
+
+  // 05. Explain a way the $time_modified might be used in a web application.
+  echo '<h3>The modified dte/time is: '.$time_modified.'<h3>';
 
 ?>
