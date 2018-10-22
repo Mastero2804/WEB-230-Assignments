e087a9c2fe824574781c489fdf520a6d01c4a1d2
brianrsavage <bsavage@racc.edu>
2018-10-21 21:34:56 -0400

    Revert "Revert "good changes""
    This reverts commit b411e80720c629b96140ceea5b336fb636fdd404.

--- series/header-redirect/index.php
+++ series/header-redirect/index.php
@@ -5,10 +5,10 @@
 <?php
 
     $redirect_page = 'http://google.com';
-	
+
     $redirect = true;
 
-    if($redirect == true)){
+    if($redirect == true){
       // 01. Explaine what is happening here.
       header('Location: '.$redirect_page);
     }
