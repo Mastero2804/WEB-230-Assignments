03e9733546f7e51d9e67f6640a4c908095edd7c2
brianrsavage <bsavage@racc.edu>
2018-10-22 07:17:33 -0400

    fixed ob_start

--- series/redirect/index.php
+++ series/redirect/index.php
@@ -1,10 +1,6 @@
-<?php
 
-  // 02. Explaine why we use ob_start.
-  ob_start(); ?>
 
 <h1>My Page</h1>
-This is my page.
 
 <?php
 
@@ -13,11 +9,8 @@ This is my page.
     $redirect = true;
 
     if($redirect == true){
-      // 02. Explaine what is happening here.
+      // 01. Explaine what is happening here.
       header('Location: '.$redirect_page);
     }
-
-    // 03. Explaine why we use ob_end_flush.
-    ob_end_flush();
 
  ?>
