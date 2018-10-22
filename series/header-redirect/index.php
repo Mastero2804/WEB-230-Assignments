ad1e0768b10cc91112885b0b8cede669a457e7ce
brianrsavage <bsavage@racc.edu>
2018-10-21 20:31:02 -0400

    fix redirect code

--- series/header-redirect/index.php
+++ series/header-redirect/index.php
@@ -5,13 +5,12 @@
 <?php
 
     $redirect_page = 'http://google.com';
-	
+
     $redirect = true;
 
-    if($redirect == true)){
+    if($redirect == true){
       // 01. Explaine what is happening here.
       header('Location: '.$redirect_page);
     }
 
  ?>
- 
