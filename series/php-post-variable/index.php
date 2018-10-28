3f1d5135f3f0614459071e87e4e6dcd3f605ce0c
brianrsavage <bsavage@racc.edu>
2018-10-27 21:02:03 -0400

    completed php-post-variable

--- series/php-post-variable/index.php
+++ series/php-post-variable/index.php
@@ -1,12 +1,5 @@
 
-<!-- Add bootstrap to give your form a little style. -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <div class="fluid-container">
-    <div class="row justify-content-center">
-      <div class="col-6" style="padding:25px;">
-        <h1>PHP $_POST Variable</h1>
-      </div>
-    </div>
+
     <?php
 
       // 01. Explain what this variable will be used for and how.
@@ -19,34 +12,17 @@
         // 02. Explain the process of the following if statment.
         if ( !empty($password)) {
           if($password == $match){
-            $msg = 'Welcome! You are now logged in.';
+            echo 'That is correct!';
           }else{
-            $msg = 'Please try again.';
-            $msgType = 'danger';
+            echo 'That is incorrect!';
           }
         }else{
-          $msg = 'Please enter a password.';
-          $msgType = 'danger';
+          echo 'Please enter a password.';
         }
-        echo '<div class="row justify-content-center"><div class="col-6"><div class="alert alert-'.$msgType.'" role="alert"><h5>'.$msg.'</h5></div></div></div>';
       }
     ?>
-    <div class="row justify-content-center">
-      <div class="col-6">
         <form action="index.php" method="POST">
-          <div class="form-group">
-            <label for="username">Username/Email Address:</label>
-            <input type="text" name="username" class="form-control" placeholder="Enter user name here." value="">
-          </div>
-          <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" name="password" class="form-control" placeholder="Enter password here." value="">
-          </div>
-          <div>
+            Password:<br>
+            <input type="password" name="password"><br><br>
             <input type="submit" class="btn" value="Submit">
-          </div>
         </form>
-      </div>
-    </div>
-
-  </div>
